945dabe4fa64f57fbc196bac3b4f0e5afdaa4d9d
Joseph Quispe <josepheqch@gmail.com>
2021-05-22 19:28:11 -0500

    added more solution in-place to Remove-Duplicates

--- Arrays/Remove-Duplicates/main.cpp
+++ Arrays/Remove-Duplicates/main.cpp
@@ -21,7 +21,7 @@ public:
             }
         }
         return nums.size() - countVal;
-    }
+    };
     // nyx's solution
     int removeDuplicates1(vector<int>& nums) {
         int countDup = 0;
@@ -33,7 +33,50 @@ public:
             }
         }
         return nums.size() - countDup;
-    }
+    };
+    // other solution
+    int removeDuplicates2(vector<int>& nums) {
+        int length = nums.size();
+
+        for (int i = length - 2; i >= 0; i--) {
+            if (nums[i] == nums[i + 1]) {
+
+                for (int j = i + 1; j < length; j++) {
+                    nums[j - 1] = nums[j];
+                }
+            
+                length--;
+            }
+        }
+        return length;
+    };
+    // other solution
+    int removeDuplicates3(vector<int>& nums) {
+        // Check for edge cases.
+        if (nums.size() == 0) {
+            return 0;
+        }
+        
+        // Use the two pointer technique to remove the duplicates in-place.
+        // The first element shouldn't be touched; it's already in its correct place.
+        int writePointer = 1;
+        // Go through each element in the Array.
+        for (int readPointer = 1; readPointer < nums.size(); readPointer++) {
+            // If the current element we're reading is *different* to the previous
+            // element...
+            if (nums[readPointer] != nums[readPointer - 1]) {
+                // Copy it into the next position at the front, tracked by writePointer.
+                nums[writePointer] = nums[readPointer];
+                // And we need to now increment writePointer, because the next element
+                // should be written one space over.
+                writePointer++;
+            }
+        }
+        
+        // This turns out to be the correct length value.
+        return writePointer;
+    };
+    
 };
 
 
@@ -43,7 +86,7 @@ int main() {
     int res;
 
     nums = {0,0,1,1,1,2,2,3,3,4};
-    res = sol.removeDuplicates1(nums);
+    res = sol.removeDuplicates3(nums);
 
     cout << "Output: " << res;
 
